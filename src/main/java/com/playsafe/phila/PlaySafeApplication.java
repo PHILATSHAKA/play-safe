47b5408f3e0044ad64a914146e18bc7cd5231957
philatshaka <phila@zenaptix.com>
2020-05-31 14:00:45 +0200

    "added spring boot class that starts up the application with a servlet initializer"

---
+++ src/main/java/com/playsafe/phila/PlaySafeApplication.java
@@ -0,0 +1,11 @@
+package com.playsafe.phila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaySafeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlaySafeApplication.class, args);
+    }
+}
